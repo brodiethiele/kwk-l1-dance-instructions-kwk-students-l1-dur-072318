56f3990111add77587d31c79ec6ca7aab8bb008c
brodiethiele <brodie.thiele@gmail.com>
2018-07-23 20:41:46 +0000

    automatically backed up by learn

---
+++ dance.rb
@@ -0,0 +1,27 @@
+def base_footwork 
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward"
+end
+def skip_step_left
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Left, Right, Left and Left"
+end
+def skip_step_right
+  puts "Lower right foot to the ground"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Right, Left, Right and Right"
+end
+
+
+
+
+
+
+
+
+
+
+
+puts "Plant legs far apart, bend knees slightly and keep posture loose"
